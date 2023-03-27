267cdd8228b9dda0c9e1ee417435b3798b7e59fc
yun-ujel <dhall2022@elam.co.uk>
2023-03-27 14:57:00 +0100

    Adding Animations
    - Reorganised Sprites and Animations so that Peted placeholders are kept in their own folder
    - Renamed PlayerAnimation to PetedAnimation
    
    - Added sprites for Walk, Run, Idle, Jump & Grab (for mind game)
    - Added .anim files for new animations
    
    - Changed ScaleToScreen to include sliders in editor
    
    - Added an annotation at the bottom of InputController to note DualShock button assignments

--- Assets/Scripts/Graphics/ScaleToScreen.cs
+++ Assets/Scripts/Graphics/ScaleToScreen.cs
@@ -4,7 +4,10 @@ public class ScaleToScreen : MonoBehaviour
 {
     // This script is intended for direct child objects of a Camera (e.g. Sprite Mask)
 
-    [SerializeField] private Vector2 screenScale = new Vector2(0.5f, 1f);
+    [SerializeField, Range(0f, 1f)] private float screenScaleX;
+    [SerializeField, Range(0f, 1f)] private float screenScaleY;
+    private Vector2 screenScale = new Vector2(0.5f, 1f);
+
     [SerializeField] private Vector2 anchoredPosition = new Vector2(-1f, 0f);
     private Vector2 screenScaleOnLastCalculation;
     private Vector2 anchoredPosOnLastCalculation;
@@ -15,6 +18,11 @@ public class ScaleToScreen : MonoBehaviour
     }
 
     private void Update()
+    {
+        screenScale = new Vector2(screenScaleX, screenScaleY);
+    }
+
+    private void LateUpdate()
     {
         if (screenScaleOnLastCalculation != screenScale || anchoredPosOnLastCalculation != anchoredPosition)
         {
