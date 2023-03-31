00c60bcb8de7bc4be801547ef351254bdc596fed
yun-ujel <dhall2022@elam.co.uk>
2023-03-31 10:21:02 +0100

    Adding Animations
    -Renamed WalkAnim to GroundAnim
    - Added a few AnimationCapability objects

--- Assets/Scripts/Graphics/Animation/GroundAnim.cs
+++ Assets/Scripts/Graphics/Animation/GroundAnim.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Walk", menuName = "Scriptable Object/Animation Handler/Ground Walk")]
-public class WalkAnim : AnimationHandler
+[CreateAssetMenu(fileName = "Ground", menuName = "Scriptable Object/Animation Handler/Ground")]
+public class GroundAnim : AnimationHandler
 {
     private Move move;
     private GroundCheck groundCheck;
@@ -11,7 +11,8 @@ public class WalkAnim : AnimationHandler
     [SerializeField, Range(0f, 10f)] private float maxAnimationSpeed = 1.6f;
 
     [Header("Active When:")]
-    [SerializeField, Range(0f, 10f)] private float xVelocityIsAbove = 0f;
+    [SerializeField, Range(0f, 10f)] private float xVelocityIsAbove = Mathf.NegativeInfinity;
+    [SerializeField, Range(0f, 10f)] private float xVelocityIsBelow = Mathf.Infinity;
 
     public override void SetCharacterAnimator(CharacterAnimation characterAnimation)
     {
@@ -24,7 +25,7 @@ public class WalkAnim : AnimationHandler
     public override bool IsAnimationValid()
     {
         return (groundCheck.OnGround && move.enabled && Mathf.Abs(cAnim.Velocity.x) > 0f
-            && Mathf.Abs(cAnim.Velocity.x) > xVelocityIsAbove) && base.IsAnimationValid();
+            && Mathf.Abs(cAnim.Velocity.x) > xVelocityIsAbove && Mathf.Abs(cAnim.Velocity.x) < xVelocityIsBelow) && base.IsAnimationValid();
     }
 
     public override float GetAnimationSpeed()
