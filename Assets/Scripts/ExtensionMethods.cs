7cdd00014548afe0531226d6022ce5cb1d097938
yun-ujel <dhall2022@elam.co.uk>
2023-04-24 21:06:51 +0100

    Changes to ScaleToScreen
    - Removed ScaleToScreen ExtensionMethod (and moved to ScaleToScreen script)
    - added mirrorXScale and mirrorYScale options

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -2,26 +2,6 @@ using UnityEngine;
 
 public static class ExtensionMethods
 {
-    public static void ScaleToScreen(this Transform transform, Vector2 screenScale, Vector2 anchors)
-    {
-        transform.LocalReset2D();
-
-        // Scales an Object's size to match the Camera, multiplied by a proportion on the X or Y Axis.
-        // Also anchors their position to a point relative to the Camera.
-
-        Vector2 scale = GetWorldSize(Camera.main);
-        transform.localScale = scale * screenScale;
-
-        Vector3 worldPosition = new Vector3
-        (
-            ((scale.x * anchors.x) + transform.localScale.x) * 0.5f,
-            ((scale.y * anchors.y) + transform.localScale.y) * 0.5f,
-            transform.localPosition.z
-        );
-
-        transform.localPosition = worldPosition;
-    }
-
     public static void LocalReset2D(this Transform transform)
     {
         // Resets Transform values to the parent Transform's values.
--- Assets/Scripts/Graphics/ScaleToScreen.cs
+++ Assets/Scripts/Graphics/ScaleToScreen.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
 
-[ExecuteInEditMode]
+[ExecuteAlways]
 public class ScaleToScreen : MonoBehaviour
 {
     // This script is intended for direct child objects of a Camera (e.g. Sprite Mask)
 
     [SerializeField, Range(0f, 1f)] private float screenScaleX = 1f;
+    [SerializeField] private bool mirrorXScale;
+
+    [Space]
+
     [SerializeField, Range(0f, 1f)] private float screenScaleY = 1f;
+    [SerializeField] private bool mirrorYScale;
+
     private Vector2 screenScale = new Vector2(0.5f, 1f);
+
+    [Space]
 
     [SerializeField] private Vector2 anchoredPosition = new Vector2(-1f, -1f);
     private Vector2 screenScaleOnLastCalculation;
     private Vector2 anchoredPosOnLastCalculation;
-
-    private void Start()
-    {
-        Rescale();
-    }
 
     private void Update()
     {
@@ -33,8 +36,34 @@ public class ScaleToScreen : MonoBehaviour
 
     private void Rescale()
     {
-        transform.ScaleToScreen(screenScale, anchoredPosition);
+        CalculateScale(screenScale);
         screenScaleOnLastCalculation = screenScale;
         anchoredPosOnLastCalculation = anchoredPosition;
+    }
+
+    private void CalculateScale(Vector2 screenScale)
+    {
+        transform.LocalReset2D();
+
+        // Scales an Object's size to match the Camera, multiplied by a proportion on the X or Y Axis.
+        // Also anchors their position to a point relative to the Camera.
+
+        Vector2 scale = Camera.main.GetWorldSize();
+        transform.localScale = scale * screenScale;        
+
+        Vector3 worldPosition = ((scale * GetAnchors()) + (transform.localScale * GetMirrors())) * 0.5f;
+        worldPosition.z = transform.localPosition.z;
+
+        transform.localPosition = worldPosition;
+    }
+
+    private Vector2 GetMirrors()
+    {
+        return new Vector2(mirrorXScale ? -1f : 1f, mirrorYScale ? -1f : 1f);
+    }
+
+    private Vector2 GetAnchors()
+    {
+        return anchoredPosition * -GetMirrors();
     }
 }
